4964993cd939d443e01acdd3e77ae0dad4477103
CodingFox2008 <74830265+CodingFox2008@users.noreply.github.com>
2020-12-14 18:02:26 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var fixedRect, movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  
+  fixedRect = createSprite(400, 200, 50, 50);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(200,200,50,50);
+  movingRect.debug = true;
+  movingRect.shapeColor = "green";
+  
+}
+
+function draw() {
+  background(0);  
+
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  if(movingRect.x - fixedRect.x <movingRect.width/2  + fixedRect.width/2 &&
+    fixedRect.x - movingRect.x <movingRect.width/2  + fixedRect.width/2 ){
+  movingRect.shapeColor = "red";
+  fixedRect.shapeColor = "red";
+  }
+
+   else{
+     movingRect.shapeColor = "green";
+     fixedRect.shapeColor = "green";
+  }
+
+  
+
+  
+  drawSprites();
+}
